866de33f2caadb97eb0ec79bc429b7b6617fee6a
welyss <welyss@163.com>
2024-09-05 14:30:20 +0800

    update

--- src/main/java/org/welyss/binlog/parser/BinlogExecutor.java
+++ src/main/java/org/welyss/binlog/parser/BinlogExecutor.java
@@ -34,238 +34,253 @@ public class BinlogExecutor {
 
 	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
-		if (args.length > 0) {
-			Options options = new Options();
-			Option databaseOpt = new Option("d", "database", true, "Database filter");
-			options.addOption(databaseOpt);
-			Option tableOpt = new Option("t", "table", true, "Table filter");
-			options.addOption(tableOpt);
-			Option startBinlogPositionOpt = new Option("j", "start-position", true,
-					"Start reading the binlog at position N");
-			options.addOption(startBinlogPositionOpt);
-			Option stopBinlogPositionOpt = new Option("k", "stop-position", true,
-					"Stop reading the binlog at position N");
-			options.addOption(stopBinlogPositionOpt);
-			Option whereOpt = new Option("w", "where", true, "Condition in where for filtering data");
-			options.addOption(whereOpt);
-			Option showPosOpt = new Option("s", "show-position", false, "Show position");
-			options.addOption(showPosOpt);
-			CommandLineParser parser = new DefaultParser();
-			HelpFormatter formatter = new HelpFormatter();
-			CommandLine cmd;
-			try {
-				cmd = parser.parse(options, args);
+		Options options = new Options();
+		Option databaseOpt = new Option("d", "database", true, "Filter: Database name");
+		options.addOption(databaseOpt);
+		Option tableOpt = new Option("t", "table", true, "Filter: Table name");
+		options.addOption(tableOpt);
+		Option whereOpt = new Option("w", "where", true, "Filter: Condition in where for filtering data, Format: @column_index=value, Sample: @1=100");
+		options.addOption(whereOpt);
+		Option startBinlogPositionOpt = new Option("j", "start-position", true, "Start reading the binlog at position <arg>");
+		options.addOption(startBinlogPositionOpt);
+		Option stopBinlogPositionOpt = new Option("k", "stop-position", true, "Stop reading the binlog at position <arg>");
+		options.addOption(stopBinlogPositionOpt);
+		Option showPosOpt = new Option("s", "show-position", false, "Show position");
+		options.addOption(showPosOpt);
+		Option helpOpt = new Option("e", "help", false, "Show help");
+		options.addOption(helpOpt);
+		CommandLineParser parser = new DefaultParser();
+		CommandLine cmd;
+		try {
+			cmd = parser.parse(options, args);
+			boolean help = cmd.hasOption(helpOpt);
+			if (help) {
+				showHelp(options);
+			} else {
 				String databaseFilter = cmd.getOptionValue(databaseOpt);
 				String tableFilter = cmd.getOptionValue(tableOpt);
 				String whereFilter = cmd.getOptionValue(whereOpt);
 				long startPos = -1;
 				try {
 					startPos = Long.parseLong(cmd.getOptionValue(startBinlogPositionOpt));
-				} catch (Exception e) {}
+				} catch (Exception e) {
+				}
 				long stopPos = Long.MAX_VALUE;
 				try {
 					stopPos = Long.parseLong(cmd.getOptionValue(stopBinlogPositionOpt));
-				} catch (Exception e) {}
+				} catch (Exception e) {
+				}
 				boolean showPosition = cmd.hasOption(showPosOpt);
-				String binlogFile = args[args.length - 1];
-				if (binlogFile != null && !binlogFile.startsWith("-")) {
-					EventDeserializer eventDeserializer = new EventDeserializer();
+				if (args.length > 0) {
+					String binlogFile = args[args.length - 1];
+					if (binlogFile != null && !binlogFile.startsWith("-")) {
+						EventDeserializer eventDeserializer = new EventDeserializer();
 //					eventDeserializer.setCompatibilityMode(EventDeserializer.CompatibilityMode.DATE_AND_TIME_AS_LONG,
 //							EventDeserializer.CompatibilityMode.CHAR_AND_BINARY_AS_BYTE_ARRAY);
-					BinaryLogFileReader reader;
-					try {
-						reader = new BinaryLogFileReader(new File(binlogFile), eventDeserializer);
+						BinaryLogFileReader reader;
 						try {
-							Map<Long, TableMapEventData> tableMapEventByTableId;
-							Field field;
+							reader = new BinaryLogFileReader(new File(binlogFile), eventDeserializer);
 							try {
-								field = eventDeserializer.getClass().getDeclaredField("tableMapEventByTableId");
-								field.setAccessible(true);
-								tableMapEventByTableId = (Map<Long, TableMapEventData>) field.get(eventDeserializer);
-							} catch (NoSuchFieldException | SecurityException | IllegalArgumentException
-									| IllegalAccessException e) {
-								System.out.println("Table map event by table id faild.");
-								return;
-							}
-							for (Event event; (event = reader.readEvent()) != null;) {
-								EventHeaderV4 eh = (EventHeaderV4) event.getHeader();
-								if (eh.getPosition() > stopPos) {
-									break;
+								Map<Long, TableMapEventData> tableMapEventByTableId;
+								Field field;
+								try {
+									field = eventDeserializer.getClass().getDeclaredField("tableMapEventByTableId");
+									field.setAccessible(true);
+									tableMapEventByTableId = (Map<Long, TableMapEventData>) field
+											.get(eventDeserializer);
+								} catch (NoSuchFieldException | SecurityException | IllegalArgumentException
+										| IllegalAccessException e) {
+									System.out.println("Table map event by table id faild.");
+									return;
 								}
-								if (startPos <= eh.getPosition() && eh.getPosition() <= stopPos) {
-									EventType type = eh.getEventType();
-									if (EventType.isRowMutation(type)) {
-										long tableId;
-										// INSERT
-										if (EventType.isWrite(type)) {
-											WriteRowsEventData writeEventData = event.getData();
-											tableId = writeEventData.getTableId();
-											// UPDATE
-										} else if (EventType.isUpdate(type)) {
-											UpdateRowsEventData updateEventData = event.getData();
-											tableId = updateEventData.getTableId();
-											// DELETE
-										} else if (EventType.isDelete(type)) {
-											DeleteRowsEventData deleteEventData = event.getData();
-											tableId = deleteEventData.getTableId();
-										} else {
-											tableId = -1;
-											System.out.println("Unknown event type.");
-										}
-										TableMapEventData tmed = tableMapEventByTableId.get(tableId);
-										String database = tmed.getDatabase();
-										String table = tmed.getTable();
-										if (databaseFilter != null && databaseFilter.trim().length() > 0
-												&& !databaseFilter.equals(database)) {
-											continue;
-										} else if (tableFilter != null && tableFilter.trim().length() > 0
-												&& !tableFilter.equals(table)) {
-											continue;
-										}
-										StringBuilder sqlBuff = new StringBuilder();
-										// where filter ready
-										int whereColIndex = -1;
-										String value = null;
-										if (whereFilter != null && whereFilter.trim().length() > 0) {
-											String[] condition = whereFilter.split("=");
-											String column = condition[0];
-											if (column != null && column.startsWith("@")) {
-												try {
-													whereColIndex = Integer.parseInt(column.substring(1));
-												} catch (NumberFormatException nfe) {
-												}
-											}
-											value = condition[1];
-										}
-										if (EventType.isUpdate(type)) {
-											UpdateRowsEventData updateEventData = event.getData();
-											// update
-											List<Entry<Serializable[], Serializable[]>> rows = updateEventData.getRows();
-											if (rows.size() > 0) {
-												
-												for (int ri = 0; ri < rows.size(); ri++) {
-													Entry<Serializable[], Serializable[]> pair = rows.get(ri);
-													boolean skip = false;
-													StringBuilder sb = new StringBuilder();
-													StringBuilder whereBuff = new StringBuilder();
-													sb.append("UPDATE `").append(database).append("`.`").append(table).append("` SET ");
-													Serializable[] before = pair.getKey();
-													Serializable[] after = pair.getValue();
-													for (int i = 0; i < before.length; i++) {
-														Object beforeVal = convertVal(before[i]);
-														Object afterVal = convertVal(after[i]);
-														if (i + 1 == whereColIndex) {
-															if (beforeVal != null && !beforeVal.toString().equals(value)) {
-																skip = true;
-																break;
-															}
-														}
-														sb.append("@").append(i + 1).append("=").append(formatVal(afterVal));
-														whereBuff.append("@").append(i + 1).append("=").append(formatVal(beforeVal));
-														if (i < before.length - 1) {
-															sb.append(",");
-															whereBuff.append(" AND ");
-														}
-													}
-													sb.append(" WHERE ").append(whereBuff);
-													if (!skip) {
-														sqlBuff.append(sb);
-														if (ri < rows.size() - 1) {
-															sqlBuff.append("\n");
-														}
-													}
-												}
-											}
-										} else {
-											List<Serializable[]> rows;
+								for (Event event; (event = reader.readEvent()) != null;) {
+									EventHeaderV4 eh = (EventHeaderV4) event.getHeader();
+									if (eh.getPosition() > stopPos) {
+										break;
+									}
+									if (startPos <= eh.getPosition() && eh.getPosition() <= stopPos) {
+										EventType type = eh.getEventType();
+										if (EventType.isRowMutation(type)) {
+											long tableId;
+											// INSERT
 											if (EventType.isWrite(type)) {
 												WriteRowsEventData writeEventData = event.getData();
-												// insert
-												rows = writeEventData.getRows();
-												for (int ri = 0; ri < rows.size(); ri++) {
-													Serializable[] row = rows.get(ri);
-													boolean skip = false;
-													StringBuilder sb = new StringBuilder();
-													sb.append("INSERT INTO `").append(database).append("`.`").append(table).append("` VALUES(");
-													for (int i = 0; i < row.length; i++) {
-														Object item = convertVal(row[i]);
-														if (i + 1 == whereColIndex) {
-															if (item != null && !item.toString().equals(value)) {
-																skip = true;
-																break;
-															}
-														}
-														sb.append(formatVal(item));
-														if (i < row.length - 1) {
-															sb.append(",");
-														}
-													}
-													sb.append(")");
-													if (!skip) {
-														sqlBuff.append(sb);
-														if (ri < rows.size() - 1) {
-															sqlBuff.append("\n");
-														}
-													}
-												}
+												tableId = writeEventData.getTableId();
+												// UPDATE
+											} else if (EventType.isUpdate(type)) {
+												UpdateRowsEventData updateEventData = event.getData();
+												tableId = updateEventData.getTableId();
+												// DELETE
 											} else if (EventType.isDelete(type)) {
 												DeleteRowsEventData deleteEventData = event.getData();
-												// delete
-												rows = deleteEventData.getRows();
-												for (int ri = 0; ri < rows.size(); ri++) {
-													Serializable[] row = rows.get(ri);
-													boolean skip = false;
-													StringBuilder sb = new StringBuilder();
-													sb.append("DELETE FROM `").append(database).append("`.`").append(table).append("` WHERE ");
-													for (int i = 0; i < row.length; i++) {
-														Object item = convertVal(row[i]);
-														if (i + 1 == whereColIndex) {
-															if (item != null && !item.toString().equals(value)) {
-																skip = true;
-																break;
+												tableId = deleteEventData.getTableId();
+											} else {
+												tableId = -1;
+												System.out.println("Unknown event type.");
+											}
+											TableMapEventData tmed = tableMapEventByTableId.get(tableId);
+											String database = tmed.getDatabase();
+											String table = tmed.getTable();
+											if (databaseFilter != null && databaseFilter.trim().length() > 0
+													&& !databaseFilter.equals(database)) {
+												continue;
+											} else if (tableFilter != null && tableFilter.trim().length() > 0
+													&& !tableFilter.equals(table)) {
+												continue;
+											}
+											StringBuilder sqlBuff = new StringBuilder();
+											// where filter ready
+											int whereColIndex = -1;
+											String value = null;
+											if (whereFilter != null && whereFilter.trim().length() > 0) {
+												String[] condition = whereFilter.split("=");
+												String column = condition[0];
+												if (column != null && column.startsWith("@")) {
+													try {
+														whereColIndex = Integer.parseInt(column.substring(1));
+													} catch (NumberFormatException nfe) {
+													}
+												}
+												value = condition[1];
+											}
+											if (EventType.isUpdate(type)) {
+												UpdateRowsEventData updateEventData = event.getData();
+												// update
+												List<Entry<Serializable[], Serializable[]>> rows = updateEventData
+														.getRows();
+												if (rows.size() > 0) {
+
+													for (int ri = 0; ri < rows.size(); ri++) {
+														Entry<Serializable[], Serializable[]> pair = rows.get(ri);
+														boolean skip = false;
+														StringBuilder sb = new StringBuilder();
+														StringBuilder whereBuff = new StringBuilder();
+														sb.append("UPDATE `").append(database).append("`.`")
+																.append(table).append("` SET ");
+														Serializable[] before = pair.getKey();
+														Serializable[] after = pair.getValue();
+														for (int i = 0; i < before.length; i++) {
+															Object beforeVal = convertVal(before[i]);
+															Object afterVal = convertVal(after[i]);
+															if (i + 1 == whereColIndex) {
+																if (beforeVal != null
+																		&& !beforeVal.toString().equals(value)) {
+																	skip = true;
+																	break;
+																}
+															}
+															sb.append("@").append(i + 1).append("=")
+																	.append(formatVal(afterVal));
+															whereBuff.append("@").append(i + 1).append("=")
+																	.append(formatVal(beforeVal));
+															if (i < before.length - 1) {
+																sb.append(",");
+																whereBuff.append(" AND ");
 															}
 														}
-														sb.append("@").append(i + 1).append("=").append(formatVal(item));
-														if (i < row.length - 1) {
-															sb.append(" AND");
+														sb.append(" WHERE ").append(whereBuff);
+														if (!skip) {
+															sqlBuff.append(sb);
+															if (ri < rows.size() - 1) {
+																sqlBuff.append("\n");
+															}
 														}
 													}
-													if (!skip) {
-														sqlBuff.append(sb);
-														if (ri < rows.size() - 1) {
-															sqlBuff.append("\n");
+												}
+											} else {
+												List<Serializable[]> rows;
+												if (EventType.isWrite(type)) {
+													WriteRowsEventData writeEventData = event.getData();
+													// insert
+													rows = writeEventData.getRows();
+													for (int ri = 0; ri < rows.size(); ri++) {
+														Serializable[] row = rows.get(ri);
+														boolean skip = false;
+														StringBuilder sb = new StringBuilder();
+														sb.append("INSERT INTO `").append(database).append("`.`")
+																.append(table).append("` VALUES(");
+														for (int i = 0; i < row.length; i++) {
+															Object item = convertVal(row[i]);
+															if (i + 1 == whereColIndex) {
+																if (item != null && !item.toString().equals(value)) {
+																	skip = true;
+																	break;
+																}
+															}
+															sb.append(formatVal(item));
+															if (i < row.length - 1) {
+																sb.append(",");
+															}
+														}
+														sb.append(")");
+														if (!skip) {
+															sqlBuff.append(sb);
+															if (ri < rows.size() - 1) {
+																sqlBuff.append("\n");
+															}
+														}
+													}
+												} else if (EventType.isDelete(type)) {
+													DeleteRowsEventData deleteEventData = event.getData();
+													// delete
+													rows = deleteEventData.getRows();
+													for (int ri = 0; ri < rows.size(); ri++) {
+														Serializable[] row = rows.get(ri);
+														boolean skip = false;
+														StringBuilder sb = new StringBuilder();
+														sb.append("DELETE FROM `").append(database).append("`.`")
+																.append(table).append("` WHERE ");
+														for (int i = 0; i < row.length; i++) {
+															Object item = convertVal(row[i]);
+															if (i + 1 == whereColIndex) {
+																if (item != null && !item.toString().equals(value)) {
+																	skip = true;
+																	break;
+																}
+															}
+															sb.append("@").append(i + 1).append("=")
+																	.append(formatVal(item));
+															if (i < row.length - 1) {
+																sb.append(" AND");
+															}
+														}
+														if (!skip) {
+															sqlBuff.append(sb);
+															if (ri < rows.size() - 1) {
+																sqlBuff.append("\n");
+															}
 														}
 													}
 												}
 											}
-										}
-										// output
-										String output = sqlBuff.toString().trim();
-										if (output.length()>0) {
-											if (showPosition) {
-												sqlBuff.insert(0, "/* POSITION:" + eh.getPosition() + " */ ");
+											// output
+											String output = sqlBuff.toString().trim();
+											if (output.length() > 0) {
+												if (showPosition) {
+													sqlBuff.insert(0, "/* POSITION:" + eh.getPosition() + " */ ");
+												}
+												System.out.println(sqlBuff);
 											}
-											System.out.println(sqlBuff);
 										}
 									}
 								}
+							} finally {
+								reader.close();
 							}
-						} finally {
-							reader.close();
-						}
-					} catch (IOException e) {
+						} catch (IOException e) {
 //				logger.error("{}", e.getMessage());
-						System.out.println(e.getMessage());
+							System.out.println(e.getMessage());
+						}
+					} else {
+						System.out.println("binlog file is required.");
 					}
 				} else {
-					System.out.println("binlog file is required.");
+					showHelp(options);
 				}
-			} catch (ParseException e) {
-				System.out.println("invalid param, reason: " + e.getMessage());
-				formatter.printHelp("param-name", options);
 			}
-		} else {
-			System.out.println("param binlog file is required");
+		} catch (ParseException e) {
+			System.out.println("invalid param, reason: " + e.getMessage());
+			showHelp(options);
 		}
 	}
 
@@ -273,7 +288,7 @@ public class BinlogExecutor {
 		Object result = null;
 		if (val != null) {
 			if (val instanceof byte[]) {
-				result = new String((byte[])val);
+				result = new String((byte[]) val);
 			} else {
 				result = val;
 			}
@@ -293,5 +308,11 @@ public class BinlogExecutor {
 			}
 		}
 		return result;
+	}
+
+	private static void showHelp(Options options) {
+		HelpFormatter formatter = new HelpFormatter();
+		formatter.printHelp("binlog-parser-tool [Options] [BinlogFileName]", "Options:", options,
+				"Sample:\n  binlog-parser-tool /tmp/binlogs/mysql-bin.001130");
 	}
 }
